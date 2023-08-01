2772f2dccf3e4f8fab02442908e3c6f5a3b78819
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-01 22:18:07 +0200

    Completed challenge 1-3

---
+++ challenge2.js
@@ -0,0 +1,10 @@
+/** 
+ * (c) ACME Inc. 2010
+ */
+
+// This is the date that a user created their account
+const date = '10/07/2014';
+
+// The line below is incorrect. "console.log()" should be used to log the value to the console.
+// Corrected line:
+console.log(date);
