efa0311ad66fcf13d1e26278f026752c185f6087
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-02 11:22:58 +0200

    fixed errors

--- challenge1.js
+++ challenge1.js
@@ -4,8 +4,6 @@ const user = 'John';
 /* This is the lastname of the user */
 const surname = 'Smith';
 
-console.log(user + ' ' + surname);
+console.log(user; surname;)
 
-/*when you run the code, it will log both the user and surname variables to the console at the same time. 
-The + operator is used to concatenate the values of user and surname,
-and the space ' ' is added to separate the first name and last name when they are printed to the console. */
+/*when you run the code, it will log both the user and surname variables to the console at the same time.*/
