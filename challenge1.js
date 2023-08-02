f5abe65b4166e76dcfaa2531d0e4a8e08c0bd0bb
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-02 10:41:02 +0200

    Spacing

--- challenge1.js
+++ challenge1.js
@@ -6,4 +6,6 @@ const surname = 'Smith';
 
 console.log(user + ' ' + surname);
 
-// when you run the code, it will log both the user and surname variables to the console at the same time. The + operator is used to concatenate the values of user and surname, and the space ' ' is added to separate the first name and last name when they are printed to the console.
+/*when you run the code, it will log both the user and surname variables to the console at the same time. 
+The + operator is used to concatenate the values of user and surname,
+and the space ' ' is added to separate the first name and last name when they are printed to the console. */
